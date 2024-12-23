dc29f6f875f561359408a499427a28d0b5c4080b
Chinnayya Chintha <135199006+chinnayyachintha@users.noreply.github.com>
2024-12-23 15:36:08 +0530

    Create import.tf

---
+++ import.tf
@@ -0,0 +1,114 @@
+import {
+  id = "paymenthMethods"
+  to = aws_lambda_function.paymenthMethods
+}
+
+import {
+  id = "reservationPaymentTransaction_dev_IHW"
+  to = aws_lambda_function.reservationPaymentTransaction_dev_IHW
+}
+
+import {
+  id = "ancillarySearch"
+  to = aws_lambda_function.ancillarySearch
+}
+
+import {
+  id = "addancillary-hoper-dev-web"
+  to = aws_lambda_function.addancillary-hoper-dev-web
+}
+
+import {
+  id = "voucherCode"
+  to = aws_lambda_function.voucherCode
+}
+
+import {
+  id = "klaviyo_event_dev"
+  to = aws_lambda_function.klaviyo_event_dev
+}
+
+import {
+  id = "flightSearch_dev_Website"
+  to = aws_lambda_function.flightSearch_dev_Website
+}
+
+import {
+  id = "accessibilityRequest_Website_Dev"
+  to = aws_lambda_function.accessibilityRequest_Website_Dev
+}
+
+import {
+  id = "baggage"
+  to = aws_lambda_function.baggage
+}
+
+import {
+  id = "voucherCode_Web_Dev"
+  to = aws_lambda_function.voucherCode_Web_Dev
+}
+
+import {
+  id = "reservationPatmentTransaction"
+  to = aws_lambda_function.reservationPatmentTransaction
+}
+
+import {
+  id = "seatSealection"
+  to = aws_lambda_function.seatSealection
+}
+
+import {
+  id = "Website_Dev_LamdaAuthoriser"
+  to = aws_lambda_function.Website_Dev_LamdaAuthoriser
+}
+
+import {
+  id = "flightSelection_dev_Web"
+  to = aws_lambda_function.flightSelection_dev_Web
+}
+
+import {
+  id = "lambdaFunction-dev"
+  to = aws_lambda_function.lambdaFunction-dev
+}
+
+import {
+  id = "flightSearch"
+  to = aws_lambda_function.flightSearch
+}
+
+import {
+  id = "createReservation_Web_Dev"
+  to = aws_lambda_function.createReservation_Web_Dev
+}
+
+import {
+  id = "test_calender"
+  to = aws_lambda_function.test_calender
+}
+
+import {
+  id = "brb_purchaseService_dev_Web"
+  to = aws_lambda_function.brb_purchaseService_dev_Web
+}
+
+import {
+  id = "Website_Dev_CalenderRates"
+  to = aws_lambda_function.Website_Dev_CalenderRates
+}
+
+import {
+  id = "flair-hopper-api"
+  to = aws_lambda_function.flair-hopper-api
+}
+
+import {
+  id = "lowFareoptions_Dev_Website"
+  to = aws_lambda_function.lowFareoptions_Dev_Website
+}
+
+import {
+  id = "JWTtoken_Website_Dev"
+  to = aws_lambda_function.JWTtoken_Website_Dev
+}
