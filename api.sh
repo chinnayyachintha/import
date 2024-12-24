3e0e7307e1c34f3a175be71feaa24f1cfbef0c5c
Chinnayya Chintha <135199006+chinnayyachintha@users.noreply.github.com>
2024-12-24 20:47:37 +0530

    Update api.sh

--- api.sh
+++ api.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Import API Gateway (REST API) - intelisys_paymentgateway_api
-terraform import aws_api_gateway_rest_api.intelisys_paymentgateway_api 3drk5yfi26
-
-# Import API Gateway Resource (/v1/token) - intelisys_paymentgateway_api
-terraform import aws_api_gateway_resource.v1_token_resource 3drk5yfi26/root/v1/token
-#!/bin/bash
-
 # Function to import API Gateway REST API and associated resources
 import_api_gateway_rest_api() {
   local api_id=$1
@@ -94,26 +87,3 @@ import_lambda_function "PaymentGateway_IntelysisToken" "arn:aws:lambda:ca-centra
 import_lambda_function "PaymentGateway_Orchestrator" "arn:aws:lambda:ca-central-1:017820679929:function:PaymentGateway_Orchestrator"
 
 echo "Import completed. Please check the generated_resources.tf for the configuration."
-
-# Import API Gateway Method (POST for /v1/token) - intelisys_paymentgateway_api
-terraform import aws_api_gateway_method.v1_token_post_method 3drk5yfi26/v1_token_resource/POST
-
-# Import API Gateway Stage (Dev) - intelisys_paymentgateway_api
-terraform import aws_api_gateway_stage.dev_stage 3drk5yfi26/Dev
-
-# Import API Gateway (HTTP API) - PaymentGateway_IntelysisToken-API
-terraform import aws_api_gateway_http_api.paymentgateway_intelysis_token_api mv0roh4dqj
-
-# Import API Gateway Resource (/PaymentGateway_IntelysisToken) - PaymentGateway_IntelysisToken-API
-terraform import aws_api_gateway_http_api_resource.paymentgateway_intelysis_token_resource mv0roh4dqj/PaymentGateway_IntelysisToken
-
-# Import API Gateway Method (ANY for /PaymentGateway_IntelysisToken) - PaymentGateway_IntelysisToken-API
-terraform import aws_api_gateway_http_api_method.paymentgateway_intelysis_token_any_method mv0roh4dqj/PaymentGateway_IntelysisToken/ANY
-
-# Import API Gateway Stage (default) - PaymentGateway_IntelysisToken-API
-terraform import aws_api_gateway_http_api_stage.default_stage mv0roh4dqj/default
-
-# Import Lambda Function - PaymentGateway_IntelysisToken
-terraform import aws_lambda_function.paymentgateway_intelysis_token_lambda PaymentGateway_IntelysisToken
-
-echo "Import completed. Please check the output above."
